fb9f114618ea6937bbb7b2d26cc25ec4fc5f986b
OnlyDropped <asheburenkov@yandex.ru>
2021-10-13 00:38:07 +0300

    added our-gallery

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ $('.our-menu__inner').slick({
   slidesToScroll: 1,
   arrows: true,
   dots: false,
-  //draggable: false,
   autoplay: true,
   autoplaySpeed: 17000,
 });
+
 $('.review__slider').slick({
   arrows: false,
   //draggable: false,
   dots: true,
-  //autoplay: true,
-  //autoplaySpeed: 8000,
+  autoplay: true,
+  autoplaySpeed: 8000,
 });
