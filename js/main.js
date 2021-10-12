339f40a0df1aae6d6c958f1b6539bc1c3d22d1fd
OnlyDropped <asheburenkov@yandex.ru>
2021-10-12 23:46:25 +0300

    added review

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ $('.our-menu__inner').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   arrows: true,
+  dots: false,
   //draggable: false,
   autoplay: true,
   autoplaySpeed: 17000,
+});
+$('.review__slider').slick({
+  arrows: false,
+  //draggable: false,
+  dots: true,
+  //autoplay: true,
+  //autoplaySpeed: 8000,
 });
