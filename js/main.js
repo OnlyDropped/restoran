106d6b0f5ff74e385044a2294da8aed54abb2cfb
OnlyDropped <asheburenkov@yandex.ru>
2021-10-16 23:03:30 +0300

    added footer our-chef our-gallery review our-menu

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ $('.our-menu__inner').slick({
   dots: false,
   autoplay: true,
   autoplaySpeed: 17000,
+  responsive: [
+    {
+      breakpoint: 321,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
 
 $('.review__slider').slick({
@@ -12,9 +22,19 @@ $('.review__slider').slick({
   //draggable: false,
   dots: true,
   autoplay: true,
-  autoplaySpeed: 8000,
+  autoplaySpeed: 5000,
+    responsive: [
+    {
+      breakpoint: 321,
+      settings: {
+        arrows: false,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
 $(".header-top__link").html("МЕНЮ");
+
 
 $(".header__btn").on("click", function () {
   $(".header-navbar").toggleClass("header-navbar--active");
